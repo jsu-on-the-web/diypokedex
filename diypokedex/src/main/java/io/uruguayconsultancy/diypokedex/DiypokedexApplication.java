438c23f61b6f4f2c5fd4f2b62079eb998bd6e615
Janet Suryadi <janet@suryadi.id.au>
2023-08-01 10:00:37 +1000

    First commit

---
+++ diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/DiypokedexApplication.java
@@ -0,0 +1,13 @@
+package io.uruguayconsultancy.diypokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiypokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiypokedexApplication.class, args);
+	}
+
+}
---
+++ diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/pokemon/Pokemon.java
@@ -0,0 +1,15 @@
+package io.uruguayconsultancy.diypokedex.pokemon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class Pokemon {
+    
+}
