58e4bd99c6223525ae47d7c45e911827496376eb
Janet Suryadi <janet@suryadi.id.au>
2023-08-01 10:43:40 +1000

    Making CreatePokemonDTO

---
+++ diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/pokemon/CreatePokemonDTO.java
@@ -0,0 +1,42 @@
+package io.uruguayconsultancy.diypokedex.pokemon;
+
+import java.util.stream.Stream;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+public class CreatePokemonDTO {
+    @NotBlank
+    @Getter
+    @Setter
+    String name;
+
+    @NotBlank
+    @Getter
+    @Setter
+    String type;
+
+    @NotBlank
+    @Getter
+    @Setter
+    float height;
+
+    @NotBlank
+    @Getter
+    @Setter
+    float weight;
+
+    @NotBlank
+    @Getter
+    @Setter
+    int[] baseStats;
+
+    public CreatePokemonDTO(String name, String type, float height, float weight, int[] baseStats) {
+        this.name = name;
+        this.type = type;
+        this.height = height;
+        this.weight = weight;
+        this.baseStats = baseStats;
+    }
+}
