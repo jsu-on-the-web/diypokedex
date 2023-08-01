be52a82aefbb16d6439a2a26bfeb0e0f087da4f4
Janet Suryadi <janet@suryadi.id.au>
2023-08-01 12:39:40 +1000

    Starting to bring together StringTrimConverter and ModelMapperConfig

---
+++ diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package io.uruguayconsultancy.diypokedex.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        return modelMapper;
+    }
+}
---
+++ diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/converters/StringTrimConverter.java
@@ -0,0 +1,16 @@
+package io.uruguayconsultancy.diypokedex.converters;
+
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+public class StringTrimConverter implements Converter<String, String> {
+    public String convert(MappingContext<String, String> context) {
+
+        if (context.getSource() == null) {
+            return null;
+        }
+
+        return context.getSource().trim();
+    }
+
+}
