020e5cbce64b50d7e05c58fb4e7d2fd1c84924dc
Janet Suryadi <janet@suryadi.id.au>
2023-08-01 21:36:12 +1000

    Altering Create DTO

--- diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/pokemon/CreatePokemonDTO.java
+++ diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/pokemon/CreatePokemonDTO.java
@@ -1,7 +1,3 @@
-package io.uruguayconsultancy.diypokedex.pokemon;
-
-import java.util.stream.Stream;
-
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,37 +6,68 @@ public class CreatePokemonDTO {
     @NotBlank
     @Getter
     @Setter
-    String name;
+    private String name;
 
     @NotBlank
     @Getter
     @Setter
-    String type;
+    private String type;
 
     @NotBlank
     @Getter
     @Setter
-    float height;
+    private float height;
 
     @NotBlank
     @Getter
     @Setter
-    float weight;
-
-    @Getter
-    @Setter
-    long preEvolutionId;
+    private float weight;
 
     @NotBlank
     @Getter
     @Setter
-    int[] baseStats;
+    private long preEvolutionId;
 
-    public CreatePokemonDTO(String name, String type, float height, float weight, int[] baseStats) {
+    @NotBlank
+    @Getter
+    @Setter
+    private int baseHp;
+
+    @NotBlank
+    @Getter
+    @Setter
+    private int baseAttack;
+
+    @NotBlank
+    @Getter
+    @Setter
+    private int baseDefense;
+
+    @NotBlank
+    @Getter
+    @Setter
+    private int baseSpAtk;
+
+    @NotBlank
+    @Getter
+    @Setter
+    private int baseSpDef;
+
+    @NotBlank
+    @Getter
+    @Setter
+    private int baseSpeed;
+
+    public CreatePokemonDTO(String name, String type, float height, float weight, int baseHp, int baseAttack, int baseDefense, int baseSpAtk, int baseSpDef, int baseSpeed) {
         this.name = name;
         this.type = type;
         this.height = height;
         this.weight = weight;
-        this.baseStats = baseStats;
+        this.baseHp = baseHp;
+        this.baseAttack = baseAttack;
+        this.baseDefense = baseDefense;
+        this.baseSpAtk = baseSpAtk;
+        this.baseSpDef = baseSpDef;
+        this.baseSpeed = baseSpeed;
     }
 }
