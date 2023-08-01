37471a7704471cad8ca4954b0b157f30bb3454f6
Janet Suryadi <janet@suryadi.id.au>
2023-08-01 10:08:53 +1000

    Filling in Pokemon.java

--- diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/pokemon/Pokemon.java
+++ diypokedex/src/main/java/io/uruguayconsultancy/diypokedex/pokemon/Pokemon.java
@@ -10,6 +10,89 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "pokemon")
 public class Pokemon {
+    /* -------------------------------------------------------------------------- */
+    /*                              Variables/Fields                              */
+    /* -------------------------------------------------------------------------- */
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    @Column(name = "name")
+    private String name;
+
+    @Getter
+    @Setter
+    @Column(name = "type")
+    private String type;
+
+    @Getter
+    @Setter
+    @Column(name = "height")
+    private float height;
+
+    @Getter
+    @Setter
+    @Column(name = "weight")
+    private float weight;
+
+    @Getter
+    @Setter
+    @Column(name = "base_hp")
+    private int baseHp;
+
+    @Getter
+    @Setter
+    @Column(name = "base_attack")
+    private int baseAttack;
+
+    @Getter
+    @Setter
+    @Column(name = "base_defense")
+    private int baseDefense;
+
+    @Getter
+    @Setter
+    @Column(name = "base_spatk")
+    private int baseSpAtk;
+
+    @Getter
+    @Setter
+    @Column(name = "base_spdef")
+    private int baseSpDef;
+
+    @Getter
+    @Setter
+    @Column(name = "base_speed")
+    private int baseSpeed;
+
+    /* -------------------------------------------------------------------------- */
+    /*                                Constructors                                */
+    /* -------------------------------------------------------------------------- */
+
+    public Pokemon() {
+
+    }
+    
+    public Pokemon(String name, String type, float height, float weight, int baseHp, int baseAttack, int baseDefense,
+            int baseSpAtk, int baseSpDef, int baseSpeed) {
+        super();
+        this.name = name;
+        this.type = type;
+        this.height = height;
+        this.weight = weight;
+        this.baseHp = baseHp;
+        this.baseAttack = baseAttack;
+        this.baseDefense = baseDefense;
+        this.baseSpAtk = baseSpAtk;
+        this.baseSpDef = baseSpDef;
+        this.baseSpeed = baseSpeed;
+    }
     
 }
