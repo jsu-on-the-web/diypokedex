e472d1b1e39d826e15117f49a1750cdbb4f4643c
Janet Suryadi <janet@suryadi.id.au>
2023-08-03 08:27:07 +1000

    Writing some tests for Pokemon Repository

---
+++ diypokedex/src/test/java/io/uruguayconsultancy/diypokedex/PokemonRepositoryTest.java
@@ -0,0 +1,32 @@
+package io.uruguayconsultancy.diypokedex;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.uruguayconsultancy.diypokedex.pokemon.Pokemon;
+import io.uruguayconsultancy.diypokedex.pokemon.PokemonRepository;
+
+@DataJpaTest
+public class PokemonRepositoryTest {
+    @Autowired
+    private PokemonRepository testRepo;
+
+    @AfterEach
+    public void tearDown() {
+        testRepo.deleteAll();
+    }
+
+    @Test
+    public void testFindAll() {
+        assertThat(testRepo.findAll()).hasSize(0);
+
+        Pokemon p1 = new Pokemon("Blorbus", "Normal", 1.0, 1.0, 10, 10, 10, 10, 10, 10);
+
+        testRepo.save(p1);
+
+        assertThat(testRepo.findAll()).hasSize(1);
+    }
+}
